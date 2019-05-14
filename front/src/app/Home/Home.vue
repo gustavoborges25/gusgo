20191acc188c508b795746002be60f50498f8d83
Gustavo Borges de Camargo <gustavo.camargo@uds.com.br>
2019-05-14 11:20:40 -0300

    #1 - Criado estrutura de pastas, inseridos pacotes utilizados.

---
+++ front/src/app/Home/Home.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .home
+    span home
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+};
+</script>
+
+<style lang="sass" scoped>
+  .home
+    height: 100%
+</style>
---
+++ front/src/app/routes.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('App/Home/Home.vue'),
+  },
+];
