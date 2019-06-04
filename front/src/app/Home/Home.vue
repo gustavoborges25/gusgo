4d71e8eacfbc489d108a6d14ca0d23a783554407
Gustavo Borges de Camargo <gustavo.camargo@uds.com.br>
2019-06-04 14:52:15 -0300

    #1 - Estrutura de pastas iniciais

--- front/src/app/Home/Home.vue
+++ front/src/app/Home/Home.vue
@@ -1,21 +1,9 @@
 <template lang="pug">
-  .home
-    layout
+  span asdas
 </template>
 
 <script>
-import Layout from 'App/Home/Layout/Layout.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    Layout,
-  },
-
 };
 </script>
-
-<style lang="sass" scoped>
-  .home
-    height: 100%
-</style>
